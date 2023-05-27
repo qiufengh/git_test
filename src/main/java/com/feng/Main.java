6c28c9b6a3e9b5ecf2dce7065009a4b55412c052
feng <qiufeng2195@163.com>
2023-05-27 15:56:42 +0800

    hot-fix commit2

--- src/main/java/com/feng/Main.java
+++ src/main/java/com/feng/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
         System.out.println("Hello Git4!!!");
+        System.out.println("hot-fix test!");
     }
 }
