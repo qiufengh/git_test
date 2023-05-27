f1f8179a0f7233eb0a0ad2449a7e35d5acc9d15b
feng <qiufeng2195@163.com>
2023-05-27 15:26:39 +0800

    first commit

---
+++ src/main/java/com/feng/Main.java
@@ -0,0 +1,11 @@
+package com.feng;
+
+/**
+ * @author 秋枫
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git1");
+    }
+}
