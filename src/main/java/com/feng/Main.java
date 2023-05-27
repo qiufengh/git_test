c1786c4d4c09f9dfc2551b68f2afc0ec2f974d12
feng <qiufeng2195@163.com>
2023-05-27 15:27:14 +0800

    third commit

--- src/main/java/com/feng/Main.java
+++ src/main/java/com/feng/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
